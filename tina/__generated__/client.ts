ba799799cc53191b72daa40a049647800a30d220
Aryan <aryan@Aryans-MacBook-Air.local>
2026-04-03 22:02:42 +0530

    fix: ensure TinaCMS admin is copied into VitePress dist output correctly

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '02d6c88452480ece2330d6ba6af65d10b77d1b17', queries,  });
+export const client = createClient({ cacheDir: '/Users/aryan/Developer/projects/docs/docs/tina/__generated__/.cache/1775233924196', url: 'https://content.tinajs.io/2.2/content/0305d6ef-9a49-4e0f-847b-1f08a146d543/github/main', token: '02d6c88452480ece2330d6ba6af65d10b77d1b17', queries,  });
 export default client;
   

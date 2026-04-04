9d219f003816f3a3a18012ccf5073ec8b79301ca
Aryan <aryan@Aryans-MacBook-Air.local>
2026-04-04 18:45:57 +0530

    feat: update TinaCloud credentials for measuremate-blog

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '02d6c88452480ece2330d6ba6af65d10b77d1b17', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'ffe0aa8bcaa50758ade1d33649f695ff6114206e', queries,  });
 export default client;
   

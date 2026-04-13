ad5dd7540c276c56b615455c4dc7d6daba40ce84
Aryan <aryanadit1407@gmail.com>
2026-04-13 21:18:01 +0530

    css not loading issue checking

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   srcDir: './',
   outDir: './build',
   assetsDir: 'assets',
-  base: '/docs',
+  base: '/docs/',
   cleanUrls: true,
   ignoreDeadLinks: true,
   lang: 'en-US',
